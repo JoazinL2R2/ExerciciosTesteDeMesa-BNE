d4c3a585b152a9b54b063fb0132726efe4d6c025
JoazinL2R2 <bilinskjoao@gmail.com>
2023-09-22 16:52:34 -0300

    TesteDeMEsa-3-1
    Agora FOI ESSE NEGOÇOOOOOOOOOO AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- TesteDeMesa-3-1.cs
+++ TesteDeMesa-3-1.cs
@@ -1,15 +1,46 @@
 using System;
+
+public class Recebe
+{
+    public static void RecebeDados(string[] args)
+    {
+        Metodo Rendimento = new Metodo();
+
+        Console.WriteLine("Informe o valor do investimento inicial:");
+        Rendimento.ValorInicial = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Informe o valor da taxa:");
+        Rendimento.Taxa = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Informe a Data Inicial (dd/mm/aaaa):");
+        string DataInicialStr = Console.ReadLine();
+        DateTime DataInicial = DateTime.ParseExact(DataInicialStr, "dd/MM/yyyy", null);
+
+        Console.WriteLine("Informe a Data Final (dd/mm/aaaa):");
+        string DataFinalStr = Console.ReadLine();
+        DateTime DataFinal = DateTime.ParseExact(DataFinalStr, "dd/MM/yyyy", null);
+
+        Rendimento.CalcularValorFinal(DataInicial, DataFinal);
+    }
+}
+
 public class Metodo
 {
     public double ValorInicial;
     public double Taxa;
-    public int Periodo;
-   
-    public void CalcularValorFinal()
+
+    public void CalcularValorFinal(DateTime DataInicial, DateTime DataFinal)
     {
+        TimeSpan span = DataFinal.Subtract(DataInicial);
+        Console.WriteLine("Time Difference (days): " + span.Days);
+        
+        int meses = span.Days / 30;
+        int diasRestantes = span.Days % 30 ;
         Taxa = Taxa / 100;
-        double ValorFinal = ValorInicial * Math.Pow(1 + Taxa, Periodo);
-        Console.WriteLine("O valor final é: " + ValorFinal);
+        double ValorFinal = ValorInicial * Math.Pow(1 + Taxa, meses);
+        ValorFinal = ValorFinal * Math.Pow(1 + Taxa, diasRestantes / 30.0);
+
+        Console.WriteLine(ValorFinal);
     }
 }
 
@@ -17,14 +48,6 @@ class Program
 {
     static void Main()
     {
-        Metodo Rendimento = new Metodo();
-        
-        Console.WriteLine("Informe o valor do investimento inicial:");
-        Rendimento.ValorInicial = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Informe o valor da taxa:");
-        Rendimento.Taxa = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Informe o período:");
-        Rendimento.Periodo = Int32.Parse(Console.ReadLine());
-        Rendimento.CalcularValorFinal();
+        Recebe.RecebeDados(null);
     }
 }
